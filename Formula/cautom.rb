969efd01a48f954dc02d255054da811feda35c85
Shiyuan Chen <martin.chen.au@gmail.com>
2026-02-19 10:53:06 +1100

    Update URL and SHA256 for Cautom formula

--- Formula/cautom.rb
+++ Formula/cautom.rb
@@ -1,8 +1,8 @@
 class Cautom < Formula
   desc "Terminal Game of Life simulator"
   homepage "https://github.com/Shiyuan-Chen-17/CAutom"
-  url "https://github.com/Shiyuan-Chen-17/CAutom/releases/download/v0.1.0/cautom-0.1.0-macos-arm64.tar.gz"
-  sha256 "SHA256_GOES_HERE"
+  url "https://github.com/Shiyuan-Chen-17/Cellular-automaton/releases/download/v0.1.0/cautom-0.1.0-macos-arm64.tar.gz"
+  sha256 "34c37dc4a81a7404327d5218a2f078d3447bb9e7f9164df7ad595ae497e7aa3d"
   version "0.1.0"
 
   def install
