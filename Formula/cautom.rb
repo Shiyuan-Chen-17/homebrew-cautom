3937b93dacb807fa0e50861bafb2c3339a538460
Shiyuan Chen <martin.chen.au@gmail.com>
2026-02-20 12:37:59 +1100

    Updated Homebrew to Cautom V1.0.0 (Finished V1 Release)

--- Formula/cautom.rb
+++ Formula/cautom.rb
@@ -1,9 +1,9 @@
 class Cautom < Formula
   desc "Terminal Game of Life simulator"
   homepage "https://github.com/Shiyuan-Chen-17/CAutom"
-  url "https://github.com/Shiyuan-Chen-17/Cellular-automaton/releases/download/v0.1.0/cautom-0.1.0-macos-arm64.tar.gz"
-  sha256 "34c37dc4a81a7404327d5218a2f078d3447bb9e7f9164df7ad595ae497e7aa3d"
-  version "0.1.0"
+  url "https://github.com/Shiyuan-Chen-17/Cellular-automaton/releases/download/v1.0.0/cautom-1.0.0-macos-arm64.tar.gz"
+  sha256 "f145485f80ccc011f01e4de76c44bdbafb206d7a87798768c912603047272100"
+  version "1.0.0"
 
   def install
     libexec.install Dir["*"]
